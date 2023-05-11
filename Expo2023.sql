ed648dd57ad1af1e5e628af6f4dd21994b6efde4
Ricardo Nicolas Melara <107507393+ElRicki0@users.noreply.github.com>
2023-05-11 15:20:21 -0600

    Actualizacion de base de dtaos Expo2023.sql
    Solamente hace falta las tablas automaticas de inicio de secion y de alteradion en alguna tabla en general

--- Expo2023.sql
+++ Expo2023.sql
@@ -25,10 +25,20 @@ emp_nombre			varchar(50) not null,
 emp_documento		varchar(8000)not null,
 emp_fecha			date,
 emp_direccion		varchar(500)not null,
+emp_telefono		varchar(10),
 idGenero			int
 foreign key references [dbo].[tbGeneros] (idGenero),
 idTipoEmpleado		int
 foreign key references [dbo].[tbTipoEmpleados] ([idTipoEmpleado])
+);
+go
+
+create table tbUsuarios(
+idUsuario			int identity (1,1) primary key,
+Usuario				varchar(50) not null,
+Contraseña			varchar(50) not null,
+idEmpleado			int
+foreign key references [dbo].[tbEmpleados]([idEmpleado])
 );
 go
 
@@ -53,12 +63,40 @@ foreign key references [dbo].[tbMarcaProductos]([idMarcaProducto]),
 Unidades			int not null,
 PrecioUnitario		money,
 idBodega			int
-foreign key references [dbo].[tbBodegas] ([idBodega])
+foreign key references [dbo].[tbBodegas] 
+([idBodega])
 );
 go
 
 /*Apartado clientes*/
+create table tbClientes(
+idCliente			int identity (1,1) primary key,
+NombreCliente		varchar(100) not null,
+EdadCliente			int not null,
+TelefonoCliente		varchar(10),
+idGenero			int
+foreign key references  [dbo].[tbGeneros]([idGenero])
+);
+go
 
+create table tbTiendas(
+idTienda			int identity(1,1) primary key,
+NombreTd		varchar(500)not null,
+DireccionTd		VARCHAR(8000)NOT NULL,
+ReferenciasLDireccionTd	varchar(1000),
+);
+go
+
+create table tbDatosTiendas(
+idDatosTienda		int identity(1,1)primary key,
+NumeroTelefonoTd	varchar(10)not null,
+PersonaDestinatarioTd	varchar(50)not null,
+idCliente			int
+foreign key references [dbo].[tbClientes] ([idCliente]),
+idTienda			int
+foreign key references [dbo].[tbTiendas]([idTienda])
+);
+go
 
 /*apartado distribucion de producto*/
 create table tbMarcasvehiculos(
@@ -70,13 +108,13 @@ go
 create table tbModelos(
 idModelo			int identity (1,1) primary key,
 Modelo				varchar(150)not null,
-a�o					int, 
+año					int, 
 idMarcavehiculo		int
 foreign key references tbMarcasvehiculos(idMarcavehiculo)
 );
 go
 
-create table tbvehiculos(
+create table tbVehiculos(
 idVehiculo			int identity (1,1) primary key,
 Matricula			varchar(10) not null,
 idModelo			int 
@@ -84,7 +122,36 @@ foreign key references [dbo].[tbModelos]([idModelo])
 );
 go
 
+create table tbMantenimiento(
+idMantenimiento		int identity (1,1) primary key,
+Taller				varchar(8000) not null,
+Mecanico			varchar(50) not null,
+TFTaller			varchar(10),
+idVehiculo			INT
+foreign key references tbVehiculos(idVehiculo)
+);
+go
+
+create table tbDistribuidores(
+idDistribuidor		int identity(1,1) primary key,
+dtbNombre			varchar(50), 
+idEmpleado			int
+foreign key references [dbo].[tbEmpleados]([idEmpleado]),
+idVehiculo			int
+foreign key references tbVehiculos(idVehiculo),
+idUsuario				int
+foreign key references [dbo].[tbUsuarios] ([idUsuario])
+);
+go
+
 create table tbBitacoraDistribuciones(
-idBitacoraDistribucion	int identity(1,1) primary key
+idBitacoraDistribucion	int identity(1,1) primary key,
+FechaEntrega			date,
+idDistribuidor			int
+foreign key references[dbo].[tbDistribuidores]([idDistribuidor]),
+idDatosTienda			int
+foreign key references[dbo].[tbDatosTiendas]([idDatosTienda]),
+idProducto				int
+foreign key references[dbo].[tbProductos]([idProducto])
 );
 go
